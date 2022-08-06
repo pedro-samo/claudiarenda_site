35643253872bd864f6136f07b69813b55646b26b
Pedro S Morais <pedro.smorais@localiza.com>
2022-08-06 14:52:45 -0300

    implementing modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,4 @@
 const initFunctions = {
-  changeImageServecice: function () {
-    const menuButtons = document.querySelectorAll(
-      '.cr-services__container_wrapper > div'
-    );
-
-    const selectImageByIndex = (index) => {
-      const image = document.querySelector('.cr-services__title_image');
-      const imagesUrls = [
-        'https://raw.githubusercontent.com/pedro-samo/claudiarenda_site/master/images/services_2.png',
-        'https://raw.githubusercontent.com/pedro-samo/claudiarenda_site/master/images/services_3.png',
-        'https://raw.githubusercontent.com/pedro-samo/claudiarenda_site/master/images/services_4.png',
-        'https://raw.githubusercontent.com/pedro-samo/claudiarenda_site/master/images/services_5.png',
-        'https://raw.githubusercontent.com/pedro-samo/claudiarenda_site/master/images/services_2.png',
-        'https://raw.githubusercontent.com/pedro-samo/claudiarenda_site/master/images/services_3.png',
-      ];
-
-      const newImage = document.createElement('img');
-      newImage.classList.add('cr-services__title_image', 'slide-in-left');
-      newImage.src = imagesUrls[index];
-
-      image.replaceWith(newImage);
-    };
-
-    menuButtons.forEach((button, index) =>
-      button.addEventListener('click', () => {
-        selectImageByIndex(index);
-      })
-    );
-  },
-
   carouselSlide: function () {
     const glide = new Glide('.glide', {
       autoplay: 4000,
@@ -38,9 +8,14 @@ const initFunctions = {
     });
     glide.mount();
   },
+
+  openModal: function () {
+    MicroModal.show('modal-1');
+  },
 };
 
 window.addEventListener('DOMContentLoaded', (event) => {
   initFunctions.changeImageServecice();
   initFunctions.carouselSlide();
+  // initFunctions.openModal();
 });
